49f771d3723b7d59e2265724e036f0b8cb664e67
Kneesal <nisalcottingham@gmail.com>
2024-03-23 19:10:58 +1300

    fix: add tuple

--- src/tuples/index.ts
+++ src/tuples/index.ts
@@ -1,11 +1,13 @@
 import { contactEtl } from './contact/contact'
 import { definitionContactEtl } from './definition/contact/contact'
 import { definitionTeamEtl } from './definition/team/team'
+import { demographicDetailsETL } from './detailSheets/detailSheets'
 import { familyEtl } from './family/family'
 
 export const tuples = [
   definitionContactEtl,
   definitionTeamEtl,
+  demographicDetailsETL,
   familyEtl,
   contactEtl
 ]
